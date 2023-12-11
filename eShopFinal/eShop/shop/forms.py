966f265b0c9e0636611eee5c16ce106591881704
Oliver Mikk <o.mikk@acg.edu>
2023-12-11 20:55:08 +0200

    Delete eShopFinal directory

--- eShopFinal/eShop/shop/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Product
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ('name', 'description', 'price', 'category', 'image')
